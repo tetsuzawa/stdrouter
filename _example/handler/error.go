e96e1230a2d8e46693e7325fb9fb80b9931646c5
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-07-05 18:21:39 +0900

    Add comment

--- _example/handler/error.go
+++ _example/handler/error.go
@@ -3,9 +3,15 @@ package handler
 import "net/http"
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+		some implementation ...
+	*/
 	http.Error(w, "Not Found", http.StatusNotFound)
 }
 
 func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+		some implementation ...
+	*/
 	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 }
